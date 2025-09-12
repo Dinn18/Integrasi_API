50be73cc7701b73b4bbabbfa00f467f50eec2296
intanrahma <intanrahmasafira@gmail.com>
2025-09-13 02:59:11 +0700

    frist commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Integrasi_API
---
+++ server.js
@@ -0,0 +1,33 @@
+let reviews = [
+    {
+        id: '1',
+        film_id: "12cfb892-aac0-4c5b-94af-521852e46d6a",
+        user : "maulidin",
+        rating: '97',
+        Comment: 'animasi terbaik sepanjang masa!'
+    },
+    
+    {
+        id: '2',
+        film_id: "2baf70d1-42bb-4437-b551-e5fed5a87abe",
+        user : "intan",
+        rating: '95',
+        Comment: 'animasi terbaik sepanjang masa!'
+    },
+    
+    {
+        id: '3',
+        film_id: "86e544fd-79de-4e04-be62-5be67d8dd92e",
+        user : "adelia",
+        rating: '95',
+        Comment: 'animasi terbaik sepanjang masa!'
+    },
+
+    {
+        id:  '4',
+        film_id: "90b72513-afd4-4570-84de-a56c312fdf81",                
+        user : "budi",
+        rating: '90',
+        Comment: 'cerita yang sangat menginspirasi!'
+    }
+];
